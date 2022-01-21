889dfa17d848a65f20768fd9efa1a19e9d3409c2
MrZ_26 <1046101471@qq.com>
2022-01-22 03:27:37 +0800

    升级class模块

--- class.lua
+++ class.lua
@@ -1,3 +1,11 @@
-return function(class)
+local CLASS={}
+
+function CLASS.simple(class)
     return setmetatable({},{__index=class})
 end
+
+function CLASS.deep(class)
+    return setmetatable(TABLE.copy(class),getmetatable(class))
+end
+
+return CLASS
