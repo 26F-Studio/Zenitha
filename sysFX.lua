61f89c591612e4b8d8998b21a6a055eeabd6c284
MrZ_26 <1046101471@qq.com>
2022-01-22 03:03:44 +0800

    移除残留

--- sysFX.lua
+++ sysFX.lua
@@ -3,8 +3,8 @@ local gc_setColor,gc_setLineWidth=gc.setColor,gc.setLineWidth
 local gc_draw,gc_line=gc.draw,gc.line
 local gc_rectangle,gc_circle=gc.rectangle,gc.circle
 
-local max,min=math.max,math.min
 local rnd=math.random
+local max,min=math.max,math.min
 local ins,rem=table.insert,table.remove
 
 local fx={}
@@ -15,25 +15,12 @@ local function _normUpdate(S,dt)
 end
 
 local FXupdate={}
-function FXupdate.badge(S,dt)
-    S.t=S.t+dt
-    if S.t<.2 then
-        S.x,S.y=S.x1-14,S.y1-14
-    elseif S.t<.8 then
-        local t=((S.t-.2)*1.6667)
-        t=(3-2*t)*t*t
-        S.x,S.y=S.x1*(1-t)+S.x2*t-14,S.y1*(1-t)+S.y2*t-14
-    else
-        S.x,S.y=S.x2-14,S.y2-14
-    end
-    return S.t>=1
-end
 FXupdate.attack=_normUpdate
 FXupdate.tap=_normUpdate
 FXupdate.ripple=_normUpdate
 FXupdate.rectRipple=_normUpdate
 FXupdate.shade=_normUpdate
-function FXupdate.cell(S,dt)
+function FXupdate.particle(S,dt)
     if S.vx then
         S.x=S.x+S.vx*S.rate
         S.y=S.y+S.vy*S.rate
@@ -42,16 +29,11 @@ function FXupdate.cell(S,dt)
             S.vy=S.vy+S.ay*S.rate
         end
     end
-    S.t=S.t+dt*S.rate
-    return S.t>1
+    return _normUpdate(S,dt)
 end
 FXupdate.line=_normUpdate
 
 local FXdraw={}
-function FXdraw.badge(S)
-    gc_setColor(1,1,1,S.t<.2 and S.t*.6 or S.t<.8 and 1 or (1-S.t)*.6)
-    gc_draw(IMG.badgeIcon,S.x,S.y)
-end
 function FXdraw.attack(S)
     gc_setColor(S.r*2,S.g*2,S.b*2,S.a*min(4-S.t*4,1))
 
@@ -94,7 +76,7 @@ function FXdraw.shade(S)
     gc_setColor(S.r,S.g,S.b,1-S.t)
     gc_rectangle('fill',S.x,S.y,S.w,S.h,2)
 end
-function FXdraw.cell(S)
+function FXdraw.particle(S)
     gc_setColor(1,1,1,1-S.t)
     gc_draw(S.image,S.x,S.y,nil,S.size,nil,S.cx,S.cy)
 end
@@ -117,17 +99,7 @@ function SYSFX.draw()
     end
 end
 
-function SYSFX.newBadge(x1,y1,x2,y2)
-    ins(fx,{
-        update=FXupdate.badge,
-        draw=FXdraw.badge,
-        t=0,
-        x=x1,y=y1,
-        x1=x1,y1=y1,
-        x2=x2,y2=y2,
-    })
-end
-function SYSFX.newAttack(rate,x1,y1,x2,y2,wid,r,g,b,a)
+function SYSFX.attack(rate,x1,y1,x2,y2,wid,r,g,b,a)
     ins(fx,{
         update=FXupdate.attack,
         draw=FXdraw.attack,
@@ -139,7 +111,7 @@ function SYSFX.newAttack(rate,x1,y1,x2,y2,wid,r,g,b,a)
         r=r,g=g,b=b,a=a,
     })
 end
-function SYSFX.newTap(rate,x,y)
+function SYSFX.tap(rate,x,y)
     local T=
     {
         update=FXupdate.tap,
@@ -150,7 +122,7 @@ function SYSFX.newTap(rate,x,y)
     }
     ins(fx,T)
 end
-function SYSFX.newRipple(rate,x,y,r)
+function SYSFX.ripple(rate,x,y,r)
     ins(fx,{
         update=FXupdate.ripple,
         draw=FXdraw.ripple,
@@ -159,7 +131,7 @@ function SYSFX.newRipple(rate,x,y,r)
         x=x,y=y,r=r,
     })
 end
-function SYSFX.newRectRipple(rate,x,y,w,h)
+function SYSFX.rectripple(rate,x,y,w,h)
     ins(fx,{
         update=FXupdate.rectRipple,
         draw=FXdraw.rectRipple,
@@ -168,7 +140,7 @@ function SYSFX.newRectRipple(rate,x,y,w,h)
         x=x,y=y,w=w,h=h,
     })
 end
-function SYSFX.newShade(rate,x,y,w,h,r,g,b)
+function SYSFX.shade(rate,x,y,w,h,r,g,b)
     ins(fx,{
         update=FXupdate.shade,
         draw=FXdraw.shade,
@@ -178,20 +150,20 @@ function SYSFX.newShade(rate,x,y,w,h,r,g,b)
         r=r or 1,g=g or 1,b=b or 1,
     })
 end
-function SYSFX.newCell(rate,image,size,x,y,vx,vy,ax,ay)
+function SYSFX.particle(rate,obj,size,x,y,vx,vy,ax,ay)
     ins(fx,{
-        update=FXupdate.cell,
-        draw=FXdraw.cell,
+        update=FXupdate.particle,
+        draw=FXdraw.particle,
         t=0,
         rate=rate*(.9+rnd()*.2),
-        image=image,size=size,
-        cx=image:getWidth()*.5,cy=image:getHeight()*.5,
+        image=obj,size=size,
+        cx=obj:getWidth()*.5,cy=obj:getHeight()*.5,
         x=x,y=y,
         vx=vx,vy=vy,
         ax=ax,ay=ay,
     })
 end
-function SYSFX.newLine(rate,x1,y1,x2,y2,r,g,b,a)
+function SYSFX.line(rate,x1,y1,x2,y2,r,g,b,a)
     ins(fx,{
         update=FXupdate.line,
         draw=FXdraw.line,
