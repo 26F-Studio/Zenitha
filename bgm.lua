3ee403e21d3b81fa136d8bd851804b60a4b1eb0f
MrZ_26 <1046101471@qq.com>
2022-01-28 21:00:03 +0800

    重构bgm模块 新增多音频同时播放 新增音量/速度/位置控制函数取代部分以前的几个方法

--- bgm.lua
+++ bgm.lua
@@ -1,41 +1,130 @@
-local lastLoaded={}
-local maxLoadedCount=3
 local nameList={}
-local SourceObjList={}
-local volume=1
+local srcLib={}
+local lastLoadNames={}
+local nowPlay={}-- Now playing name(s) & source(s), like {{name='foo',source=bar},...}
+
 local defaultBGM=false
-local onChange=NULL
+local maxLoadedCount=3
+local volume=1
 
-local BGM={
-    onChange=NULL,
-    -- nowPlay=[str:playing ID]
-    -- playing=[src:playing SRC]
-    -- lastPlayed=[str:lastPlayed ID]
-}
-
-local function _tryReleaseSources()
-    local n=#lastLoaded
-    while #lastLoaded>maxLoadedCount do
-        local name=lastLoaded[n]
-        if SourceObjList[name].source:isPlaying() then
+local function _updateSources()
+    local n=#lastLoadNames
+    while #lastLoadNames>maxLoadedCount do
+        local name=lastLoadNames[n]
+        if srcLib[name].source:isPlaying() then
             n=n-1
             if n<=0 then return end
         else
-            SourceObjList[name].source=SourceObjList[name].source:release() and nil
-            table.remove(lastLoaded,n)
+            srcLib[name].source=srcLib[name].source:release() and nil
+            table.remove(lastLoadNames,n)
             return
         end
     end
 end
 local function _addFile(name,path)
-    if not SourceObjList[name] then
+    if not srcLib[name] then
         table.insert(nameList,name)
-        SourceObjList[name]={path=path,source=false}
+        srcLib[name]={
+            name=name,path=path,source=false,
+            vol=0,volChanging=false,
+            pitch=1,pitchChanging=false,
+        }
+    end
+end
+local function _tryLoad(name)
+    if srcLib[name] then
+        if srcLib[name].source then
+            return true
+        elseif love.filesystem.getInfo(srcLib[name].path) then
+            srcLib[name].source=love.audio.newSource(srcLib[name].path,'stream')
+            srcLib[name].source:setLooping(true)
+            table.insert(lastLoadNames,1,name)
+            _updateSources()
+            return true
+        else
+            LOG("No BGM: "..srcLib[name],5)
+        end
+    elseif name then
+        LOG("No BGM: "..name,5)
     end
 end
 
+local function task_setVolume(obj,ve,time,stop)
+    local vs=obj.vol
+    local t=0
+    while true do
+        t=math.min(t+coroutine.yield(),time)
+        local v=MATH.lerp(vs,ve,t/time)
+        obj.source:setVolume(v)
+        obj.vol=v
+        if t==time then
+            if v==0 and stop then
+                obj.source:stop()
+            end
+            obj.volChanging=false
+            return true
+        end
+    end
+end
+local function clearVolumeTask(obj)
+    TASK.removeTask_iterate(function(task)
+        return task.code==task_setVolume and task.args[1]==obj
+    end,obj)
+end
+
+local function task_setPitch(obj,pe)
+    local ps=obj.pitch
+    local t=0
+    while true do
+        t=math.min(t+coroutine.yield(),1)
+        local p=MATH.lerp(ps,pe,t)
+        obj.source:setPitch(p)
+        obj.pitch=p
+        if t==1 then
+            obj.pitchChanging=false
+            return true
+        end
+    end
+end
+local function clearPitchTask(obj)
+    TASK.removeTask_iterate(function(task)
+        return task.code==task_setPitch and task.args[1]==obj
+    end,obj)
+end
+
+local BGM={}
+
 function BGM.getList() return nameList end
 function BGM.getCount() return #nameList end
+
+function BGM.setDefault(bgms)
+    if type(bgms)=='string' then
+        bgms={bgms}
+    elseif type(bgms)=='table' then
+        for i=1,#bgms do assert(type(bgms[i])=='string',"BGM list must be list of strings") end
+    else
+        error("BGM.play(name,args): name must be string or table")
+    end
+    defaultBGM=bgms
+end
+function BGM.setMaxSources(count)
+    assert(type(count)=='number' and count>0 and count%1==0,"BGM.setMaxSources(count): count must be a positive integer")
+    maxLoadedCount=count
+    _updateSources()
+end
+function BGM.setVol(vol)
+    assert(type(vol)=='number' and vol>=0 and vol<=1,"BGM.setVol(vol): count must be in range 0~1")
+    volume=vol
+    for i=1,#nowPlay do
+        local np=nowPlay[i]
+        if not np.volChanging then
+            np.source:setVolume(vol)
+            if vol==0 then
+                np.source:pause()
+            end
+        end
+    end
+end
 function BGM.load(name,path)
     if type(name)=='table' then
         for k,v in next,name do
@@ -48,143 +137,110 @@ function BGM.load(name,path)
     LOG(BGM.getCount().." BGM files added")
 end
 
-function BGM.setDefault(bgm)
-    defaultBGM=bgm
-end
-function BGM.setMaxSources(count)
-    maxLoadedCount=count
-    _tryReleaseSources()
-end
-function BGM.setChange(func)
-    assert(type(func)=='function','BGM.setChange(func) func must be function')
-    onChange=func
-end
-function BGM.setVol(v)
-    assert(type(v)=='number' and v>=0 and v<=1,'Wrong volume')
-    volume=v
-    if BGM.playing then
-        if volume>0 then
-            BGM.playing:setVolume(volume)
-            BGM.playing:play()
-        elseif BGM.nowPlay then
-            BGM.playing:pause()
-        end
-    end
-end
+function BGM.play(bgms,args)
+    if not args then args='' end
+    if not bgms then bgms=defaultBGM end
+    if not bgms then return end
 
-local function task_fadeOut(src)
-    while true do
-        coroutine.yield()
-        local v=src:getVolume()-volume/40
-        src:setVolume(v>0 and v or 0)
-        if v<=0 then
-            src:stop()
-            return true
-        end
-    end
-end
-local function task_fadeIn(src)
-    while true do
-        coroutine.yield()
-        local v=volume
-        v=math.min(v,src:getVolume()+v/40)
-        src:setVolume(v)
-        if v>=volume then
-            return true
-        end
-    end
-end
-local function check_curFadeOut(task,code,src)
-    return task.code==code and task.args[1]==src
-end
-local function _tryLoad(name)
-    if SourceObjList[name] then
-        if SourceObjList[name].source then
-            return true
-        elseif love.filesystem.getInfo(SourceObjList[name].path) then
-            SourceObjList[name].source=love.audio.newSource(SourceObjList[name].path,'stream')
-            SourceObjList[name].source:setVolume(0)
-            table.insert(lastLoaded,1,name)
-            _tryReleaseSources()
-            return true
-        else
-            LOG("No BGM: "..SourceObjList[name],5)
-        end
-    elseif name then
-        LOG("No BGM: "..name,5)
-    end
-end
-function BGM.play(name,args)
-    name=name or defaultBGM
-    args=args or ''
-    if not _tryLoad(name) or args:sArg('-preLoad') then return end
-    if volume==0 then
-        BGM.nowPlay=name
-        BGM.playing=SourceObjList[name].source
-        return true
-    end
-    if name and SourceObjList[name].source then
-        if BGM.nowPlay~=name then
-            if BGM.nowPlay then
-                if not args:sArg('-sdout') then
-                    TASK.new(task_fadeOut,BGM.playing)
-                else
-                    BGM.playing:pause()
-                end
-            end
-            TASK.removeTask_iterate(check_curFadeOut,task_fadeOut,SourceObjList[name].source)
-            TASK.removeTask_code(task_fadeIn)
+    if type(bgms)=='string' then bgms={bgms} end
+    -- if TABLE.compare(nowPlay,bgms) then return end
 
-            BGM.nowPlay=name
-            BGM.playing=SourceObjList[name].source
-            if not args:sArg('-sdin') then
-                BGM.playing:setVolume(0)
-                TASK.new(task_fadeIn,BGM.playing)
-            else
-                BGM.playing:setVolume(volume)
-                BGM.playing:play()
-            end
-            SourceObjList[name].source:setLooping(not args:sArg('-noloop'))
-            BGM.lastPlayed=BGM.nowPlay
-            BGM.playing:play()
-            onChange(name)
-        end
-        return true
-    end
-end
-function BGM.setPitch(t)
-    if BGM.playing then
-        BGM.playing:setPitch(t)
-    end
-end
-function BGM.seek(t)
-    if BGM.playing then
-        BGM.playing:seek(t)
-    end
-end
-function BGM.isPlaying()
-    return BGM.playing and BGM.playing:isPlaying()
-end
-function BGM.continue()
-    if BGM.lastPlayed then
-        BGM.nowPlay,BGM.playing=BGM.lastPlayed,SourceObjList[BGM.lastPlayed].source
-        TASK.removeTask_iterate(check_curFadeOut,task_fadeOut,SourceObjList[BGM.nowPlay].source)
-        TASK.removeTask_code(task_fadeIn)
-        TASK.new(task_fadeIn,BGM.playing)
-        BGM.playing:play()
+    assert(type(bgms)=='table',"BGM.play(name,args): name must be string or table")
+
+    BGM.stop()
+
+    for i=1,#bgms do
+        local bgm=bgms[i]
+        if type(bgm)~='string' then error("BGM list can only be list of string") end
+        if not _tryLoad(bgm) or STRING.sArg(args,'-preLoad') then goto _CONTINUE_ end
+
+        local obj=srcLib[bgms[i]]
+        obj.vol=1
+        obj.pitch=1
+        obj.volChanging=false
+        obj.pitchChanging=false
+
+        local source=obj.source
+        source:seek(0)
+        source:setPitch(1)
+        source:setVolume(volume)
+        source:play()
+
+        table.insert(nowPlay,obj)
+        clearVolumeTask(obj)
+
+        ::_CONTINUE_::
     end
 end
 function BGM.stop(args)
-    args=args or ''
-    TASK.removeTask_code(task_fadeIn)
-    if not args:sArg('-s') then
-        if BGM.nowPlay then
-            TASK.new(task_fadeOut,BGM.playing)
+    if not args then args='' end
+
+    if #nowPlay>0 then
+        for i=1,#nowPlay do
+            local obj=nowPlay[i]
+            clearVolumeTask(obj)
+            if STRING.sArg(args,'-sdout') then
+                obj.source:stop()
+                obj.volChanging=false
+            else
+                TASK.new(task_setVolume,obj,0,false,true)
+                obj.volChanging=true
+            end
         end
-    elseif BGM.playing then
-        BGM.playing:pause()
+        TABLE.cut(nowPlay)
     end
-    BGM.nowPlay=false
-    BGM.playing=false
 end
+function BGM.set(bgms,mode,...)
+    if type(bgms)=='string' then
+        if bgms=='all' then
+            bgms=nowPlay
+        else
+            bgms={srcLib[bgms]}
+        end
+    elseif type(bgms)=='table' then
+        for i=1,#bgms do
+            assert(type(bgms[i])=='string',"BGM list must be list of strings")
+            bgms[i]=srcLib[bgms[i]]
+        end
+    else
+        error("BGM.play(name,args): name must be string or table")
+    end
+    for i=1,#bgms do
+        local obj=bgms[i]
+        if obj.source then
+            if mode=='volume' then
+                clearVolumeTask(obj)
+
+                local vol,time=...
+                if not time then time=1 end
+
+                assert(type(vol)=='number' and vol>=0 and vol<=1,"BGM.set(...,volume): volume must be in range 0~1")
+                assert(type(time)=='number' and time>=0,"BGM.set(...,time): time must be positive number")
+
+                TASK.new(task_setVolume,obj,vol,time)
+            elseif mode=='pitch' then
+                clearPitchTask(obj)
+
+                local pitch,time=...
+                if not pitch then pitch=1 end
+                if not time then time=1 end
+
+                assert(type(pitch)=='number' and pitch>0 and pitch<=32,"BGM.set(...,pitch): pitch must be in range 0~32")
+                assert(type(time)=='number' and time>=0,"BGM.set(...,time): time must be positive number")
+
+                TASK.new(task_setPitch,obj,pitch,time)
+            elseif mode=='seek' then
+                local time=...
+                assert(type(time)=='number' and time>=0 and time<=obj.source:getDuration(),"BGM.set(...,time): time must be in range 0~[song length]")
+                obj.source:seek(...)
+            else
+                error("BGM.set(...,mode): mode must be 'volume', 'pitch', or 'seek'")
+            end
+        end
+    end
+end
+function BGM.isPlaying()
+    return #nowPlay>0
+end
+
 return BGM
