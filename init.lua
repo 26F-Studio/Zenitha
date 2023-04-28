09c23972b3a628d6b0fe81c7f019ff0321162736
MrZ_26 <1046101471@qq.com>
2023-04-28 18:10:11 +0800

    微调注释

--- init.lua
+++ init.lua
@@ -778,14 +778,14 @@ function love.run()
                 gc_replaceTransform(SCR.xOy_ul)
                     MES_draw()
                 gc_replaceTransform(SCR.xOy_d)
-                    -- Draw Version string
+                    -- Version string
                     gc_setColor(.9,.9,.9,.42)
                     setFont(20,'_norm')
                     gc_printf(versionText,-2600,-30,5200,'center')
                 gc_replaceTransform(SCR.xOy_dl)
                     local safeX=SCR.safeX/SCR.k
 
-                    -- Draw FPS
+                    -- FPS
                     setFont(15,'_norm')
                     gc_setColor(COLOR.L)
                     gc_print(FPS(),safeX+5,-20)
