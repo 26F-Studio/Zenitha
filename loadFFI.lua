c3c464215a33f5b2546075a084c06dbcc0f8e098
MrZ_26 <1046101471@qq.com>
2024-11-19 06:07:12 +0800

    LOADFFI模块用法换成类似xpcall的错误处理器并提供一个默认的函数

--- loadFFI.lua
+++ loadFFI.lua
@@ -10,11 +10,17 @@ if SYSTEM=='Linux' then
     end
 end
 
----
+local function defaultErrorHandler(errInfo)
+    for i=1,#errInfo do
+        MSG.errorLog(errInfo[i])
+    end
+end
+
+---A wrapped ffi.load that will try to load FMOD library in different paths with an error handler receiving all error informations
 ---@param name string name of the library, 'fmod' for 'fmod.dll'|'libfmod.so'
----@return ffi.namespace*
----@overload fun(name:string):result:false,errInfo:string[]
-return function(name)
+---@param handler? fun(errLog:string[]) will call this if loading failed
+---@return ffi.namespace*|false
+return function(name,handler)
     local errLog={}
     for i=1,#path do
         local suc,res
@@ -22,8 +28,9 @@ return function(name)
         if suc then
             return res
         else
-            table.insert(errLog,"Loading FMOD lib:"..res)
+            table.insert(errLog,STRING.repD("ffi.load('$1'): $2",name,res))
         end
     end
-    return false,errLog
+    (type(handler)=='function' and handler or defaultErrorHandler)(errLog)
+    return false
 end
